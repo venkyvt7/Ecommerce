b01db34069a2a15a1fd52e6cadd1b4945c434220
venkatesh tripathi <venkateshp.tripathi@gmail.com>
2021-07-22 23:55:39 +0530

    added resposive navbar css logic

--- frontend/ecommerce/src/nav/navbar.js
+++ frontend/ecommerce/src/nav/navbar.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './navbar.css';
 function Navbar() {
+
+const [butt,setButt]=useState(1);
+
+
+
     return (
         <div>
            <div>
                 <nav>
-        
+       
         <ul>
-                <li> home</li>
+       <li>  <button onClick={()=>{setButt(1-butt)}}> open </button></li> 
+           <div className="links" id={butt?"hidden":"show"}>   <li> home</li>
                 <li> view products</li>
                 <li> mycart</li>
                 <li> myorders</li>
                 <li>personal details</li>
-       
+                </div>  
        
         </ul>
 
