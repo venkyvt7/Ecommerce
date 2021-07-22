d7364453bd4ba0720e55840db334f1036e6bdae3
venkatesh tripathi <venkateshp.tripathi@gmail.com>
2021-07-22 22:34:36 +0530

    making resposive navbar

--- frontend/ecommerce/src/App.js
+++ frontend/ecommerce/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './entry/login';
+import Navbar from './nav/navbar';
 function App() {
   return (
     <div className="App">
-      <Login/>
+      {/* <Login/> */}
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ frontend/ecommerce/src/nav/navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './navbar.css';
+function Navbar() {
+    return (
+        <div>
+           <div>
+                <nav>
+        
+        <ul>
+                <li> home</li>
+                <li> view products</li>
+                <li> mycart</li>
+                <li> myorders</li>
+                <li>personal details</li>
+       
+       
+        </ul>
+
+                </nav>
+           </div>
+        </div>
+    )
+}
+
+export default Navbar
