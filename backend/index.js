25559b0162f56a06e7eac4ff3b8b844e738fe362
venkatesh tripathi <venkateshp.tripathi@gmail.com>
2021-07-22 15:49:38 +0530

    organizing folders

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,16 @@
 const express=require('express');
+const app=express();
+const PORT=process.env.PORT||4001;
+const users=require('./routes/userentry/user')
 
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+
+res.send("100% working");
+
+})
+
+
+app.listen(PORT);
