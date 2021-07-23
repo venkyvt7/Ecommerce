60ac0bdd4cc2638fe2eeb1af4f387e98ad3bd904
venkatesh tripathi <venkateshp.tripathi@gmail.com>
2021-07-23 12:35:41 +0530

    setup redux

--- frontend/ecommerce/src/App.js
+++ frontend/ecommerce/src/App.js
@@ -1,9 +1,9 @@
 // import logo from './logo.svg';
 // import './App.css';
-import Login from './entry/login';
-import Navbar from './nav/navbar';
-import Orders from './orders/orders';
-import Products from './products/products';
+import Login from './components/entry/login';
+import Navbar from './components/nav/navbar';
+import Orders from './components/orders/orders';
+import Products from './components/products/products';
 function App() {
   return (
     <div  className="App" >
---
+++ frontend/ecommerce/src/redux/shopping/shopping-action.js
@@ -0,0 +1,35 @@
+import * as actionTypes from './shopping-types';
+
+
+export const addToCart=(itemId)=>{
+return 
+{
+    type:actionTypes.ADD_TO_CART,
+    payload:{
+        id:itemID
+    }
+
+}
+
+
+}
+
+
+export const removeFromCart=(itemID)=>{
+return 
+{
+  type:actionTypes.REMOVE_FROM_CART,
+  payload:{
+      id:itemID
+  }
+
+
+
+}
+
+
+
+}
+
+
+export const 
